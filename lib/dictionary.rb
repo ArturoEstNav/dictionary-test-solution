1f6f87d6c1f11914009606bb898626b54d075924
arturo estrada <arturoestradanav@gmail.com>
2022-06-18 10:01:01 -0500

    Revert "Add Dictionary class and find_meaning method"
    This reverts commit 1889683fedfc1f3eef6f0f8f5e5ec18350d232f1.

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,12 +1,1 @@
 # Escribe tu código del ejercicio en este archivo. Haz que las pruebas en test/dictionary_test.rb pasen
-class Dictionary
-  def initialize(word_list)
-    @word_list = word_list
-  end
-
-  def find_meaning(word)
-    match = @word_list.select { |element| element.word == word }.first
-
-    match ? match.meaning : "We don't have that word yet"
-  end
-end
