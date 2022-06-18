ec7b4ae3667ca0f45e6d9cf90c63dfd5aa039a11
arturo estrada <arturoestradanav@gmail.com>
2022-06-18 10:03:13 -0500

    Add Dictionary class and find_meaning method

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,1 +1,11 @@
-# Escribe tu código del ejercicio en este archivo. Haz que las pruebas en test/dictionary_test.rb pasen
+class Dictionary
+  def initialize(word_list)
+    @word_list = word_list
+  end
+
+  def find_meaning(word)
+    match = @word_list.find { |element| element.word == word }
+
+    match ? match.meaning : "We don't have that word yet"
+  end
+end
